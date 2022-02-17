1621d9d7d8b5def987856d0ed7bddbf6218e3d05
Filippo Ranza <filipporanza@gmail.com>
2022-02-17 10:53:15 +0100

    Add load data from HDF5 File

--- main.jl
+++ main.jl
@@ -1,10 +1,14 @@
 using Plots
 
 include("gen_clients.jl")
+include("load_data.jl")
 
+
+data = load_data("lausanne-bus-data.hdf5")
 
 map_size = 150;
-counts = [ 2022 886 137 265 1063 92 183 3230 5184]
+counts = get_data(data, "13", "sw-mon", "avg")
 clients = generate_clients(counts, map_size, .1, .5, 2)
 println(sum(clients), " ", length(clients[clients .> 0]))
+close_data(data)
 contour(1:map_size, 1:map_size, clients)
